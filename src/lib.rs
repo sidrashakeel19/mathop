98fa735dcf620842ae0948857c8a3108ef678ad7
sidrashakeel19 <55503447+sidrashakeel19@users.noreply.github.com>
2019-10-16 22:55:26 +0500

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+mod simp_op {
+    mod sum {
+        pub fn isum(iin1: i32, iin2: i32) -> i32 {
+            iin1 + iin2
+        } 
+
+        pub fn fsum(fin1: f32, fin2: f32) -> f32 {
+            fin1 + fin2
+        }
+    }
+
+    mod diff {
+        pub fn idiff(iin1: i32, iin2: i32 ) -> i32 {
+            iin1 - iin2
+        }
+        pub fn fdiff(fin1: f32, fin2: f32) -> f32 {
+            fin1 - fin2
+        }
+    }
+
+    mod multiply {
+        pub fn imul(iin1: i32, iin2: i32) -> i32 {
+            iin1 * iin2
+        }
+        pub fn fmul(fin1: f32, fin2: f32) -> f32 {
+            fin1 * fin2
+        }
+    }
+}
